5894292e03a13e441e0bf3d90e9dd4f3b558d7ce
Song. <zjs849437698@163.com>
2020-03-23 03:57:39 +0100

    afdss

--- src/test/java/calc/CaculatorTest.java
+++ src/test/java/calc/CaculatorTest.java
@@ -41,14 +41,7 @@ public class CaculatorTest {
 		Assert.assertEquals(result, calc.add(a, b));
 	}
 	
-	@Test
-	public void testAdd4() {
-		int a=1;
-		int b=3;
-		int result=4;
 
-		Assert.assertEquals(result, calc.add(a, b));
-	}
 	
 
 	
