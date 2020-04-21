cd4136e9eae4957152e07d1b2ca1b20c9b68a908
Song. <zjs849437698@163.com>
2020-04-22 01:08:42 +0200

    zjszjs

--- src/test/java/calc/CaculatorTest.java
+++ src/test/java/calc/CaculatorTest.java
@@ -29,7 +29,7 @@ public class CaculatorTest {
 	public void testDiv() {
 		assertEquals(1, calc.div(2,2));
 	}
-	@Test
+	@Test(expected = java.lang.ArithmeticException.class)
 	public void testDivByZero() {
 		assertEquals(0, calc.div(3,0));
 	}
